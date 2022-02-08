afee273cf7a98c01a8925011d1bc55a35a7ea4d8
sohamsshah <sohamshah456@gmail.com>
2022-02-08 17:33:11 +0530

    add transaction apis

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,8 +1,15 @@
 import prisma from "../../../lib/prisma";
 
+// Transaction: https://www.prisma.io/docs/guides/performance-and-optimization/prisma-client-transactions-guide#transaction-api
+
 export default async function handle(req, res) {
   const { title, content, email } = req.body;
-  const result = await prisma.post.create({
+
+  const feed = await prisma.post.findMany({
+    where: { published: true },
+  });
+  const newContent = feed.map((item) => item.title).join("\n");
+  const createPost = prisma.post.create({
     data: {
       title: title,
       content: content,
@@ -10,5 +17,16 @@ export default async function handle(req, res) {
       published: true,
     },
   });
+  const updateAllPosts = prisma.post.updateMany({
+    where: {
+      title: "All Posts",
+    },
+    data: {
+      content: newContent,
+    },
+  });
+
+  const result = await prisma.$transaction([createPost, updateAllPosts]);
+  console.log(result);
   res.json(result);
 }
